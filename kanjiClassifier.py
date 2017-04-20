eaa046eda2ead14f591e195eb3c087019f9ae652
ChetBacon <tensen.mark@gmail.com>
2017-04-20 09:48:37 +0200

    kakko

--- kanjiClassifier.py
+++ kanjiClassifier.py
@@ -3,15 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def compute_accuracy(y_hat_val, batch_label):
+    pred_cls = np.argmax(y_hat_val, axis=1)
+    act_cls = np.argmax(batch_label, axis=1)
+    acc = np.sum(pred_cls == act_cls)
+
+    return acc
+
+
 def get_batch(tr_image, tr_labels, batch_size):
-    seek_sample = get_batch.seek # current position of the seek pointer
+    seek_sample = get_batch.seek  # current position of the seek pointer
     n_sample = tr_image.shape[0]
 
     # select samples based on the current seek pointer and the batch_size
-    sel_samples = np.arange(seek_sample, seek_sample+batch_size)
+    sel_samples = np.arange(seek_sample, seek_sample + batch_size)
     # reset to index zero when end of data set is reached
     sel_samples[sel_samples >= n_sample] -= n_sample
-    batch = tr_image[sel_samples, :, :, :] # create the batch
+    batch = tr_image[sel_samples, :, :, :]  # create the batch
     batch_label = tr_labels[sel_samples, :]
 
     # update seek pointer
@@ -21,12 +29,24 @@ def get_batch(tr_image, tr_labels, batch_size):
 
     return batch, batch_label
 
-def compute_accuracy(y_hat_val, batch_label):
-    pred_cls = np.argmax(y_hat_val, axis=1)
-    act_cls = np.argmax(batch_label, axis=1)
-    acc = np.sum(pred_cls == act_cls)
 
-    return acc
+def get_batch_tst(tr_image, tr_labels, batch_size):
+    seek_sample = get_batch_tst.seek  # current position of the seek pointer
+    n_sample = tr_image.shape[0]
+
+    # select samples based on the current seek pointer and the batch_size
+    sel_samples = np.arange(seek_sample, seek_sample + batch_size)
+    # reset to index zero when end of data set is reached
+    sel_samples[sel_samples >= n_sample] -= n_sample
+    batch = tr_image[sel_samples, :, :, :]  # create the batch
+    batch_label = tr_labels[sel_samples, :]
+
+    # update seek pointer
+    get_batch_tst.seek += batch_size
+    if get_batch_tst.seek >= n_sample:
+        get_batch_tst.seek -= n_sample
+
+    return batch, batch_label
 
 
 def create_graph(params):
@@ -85,7 +105,6 @@ def main(params):
     batch_size = params['batch_size']
     inp_img, y, y_hat, loss, step, keep_prob = create_graph(params)
     destination_location = params['destination_location']
-    dropout_val = params['dropout_prob']
     outputfile = params['outputfile']
     keep_prob_val = params['keep_prob']
 
@@ -98,10 +117,65 @@ def main(params):
     # load img data
     images = np.load('img_data/images_sample.dat')
     labels = np.load('img_data/labels_sample.dat')
+    images_trn = images[0:80]
+    labels_trn = labels[0:80]
+    images_tst = images[80:100]
+    labels_tst = labels[80:100]
 
     # set the index of the first sample to be read from the data
     get_batch.seek = 0
     acc = 0
+
+
+
+
+    tr_data, tr_labels = load_cifar(norm_image=True)
+    tst_data, tst_labels = load_cifar_test(norm_image=True)
+    acc = 0
+    for e in range(n_epoch):
+
+        if e % 100 == 0:
+            print('Accuracy = ' + str(acc * 100 / 10000))
+            acc = 0
+
+        batch, batch_label = get_batch(tr_data, tr_labels, batch_size)
+
+        # run one step of optimizer
+        y_hat_val, loss_val, _ = sess.run([y_hat, loss, step], feed_dict={inp_img: batch,
+                                                                          y: batch_label, keep_prob: dropout_val})
+
+        # compute accuracy
+        acc += compute_accuracy(y_hat_val, batch_label)
+
+        print('Error in epoch ' + str(e) + ' = ' + str(np.sum(loss_val)))
+
+        # test accuracy
+        if e % 200 == 0:
+            # test accuracy on separate batch
+            get_batch_tst.seek = 0
+            tst_acc = 0
+
+            for f in range(100):
+                batch, batch_label = get_batch_tst(tst_data, tst_labels, batch_size)
+
+                # run one step of optimizer
+                y_hat_val = sess.run(y_hat, feed_dict={inp_img: batch, y: batch_label, keep_prob: 1})
+
+                # compute accuracy
+                tst_acc += compute_accuracy(y_hat_val, batch_label)
+
+            print('accuracy on tstbtch is ' + str(tst_acc * 100 / 10000))
+
+
+
+
+
+
+
+
+
+
+
 
     # train the network
     for i in range(n_epoch):
@@ -109,18 +183,17 @@ def main(params):
         # load data and labels
 
         # load batch images and labels into batch
-        batch, batch_label = get_batch(images, labels, batch_size)
+        batch, batch_label = get_batch(images_trn, labels_trn, batch_size)
 
         # do a training step
         y_hat_val, loss_val, _ = sess.run([y_hat, loss, step], feed_dict={inp_img:batch, y:batch_label, keep_prob:keep_prob_val})
 
-        acc += compute_accuracy(y_hat_val, batch_label)
-
-        if i % 10 == 0:
+        # evaluate
+        if i % 40 == 0:
+            batch_tst, batch_label_tst = get_batch_tst(images_tst, labels_tst, len(labels_tst))
+            y_hat_tst_val = sess.run(y_hat, feed_dict={inp_img:batch_tst, y:batch_label_tst, keep_prob:1.0})
+            acc = compute_accuracy(y_hat_tst_val, batch_label_tst)
             print('accuracy in ' + str(i) + ' itterations is ' + str(acc) + ' this is on epoch ' + str(loss_val))
-
-            acc = 0
-
 
 
 if __name__ == '__main__':
@@ -128,16 +201,15 @@ if __name__ == '__main__':
     params['input_dim'] = 48
     params['n_class'] = 40
     params['n_inp_channel'] = 1
-    params['n_filter'] = [64, 64, 64, 64]
+    params['n_filter'] = [16, 16, 16, 16]
     params['filter_dim'] = [5, 3, 5, 3]
     params['conv_strides'] = [1, 2, 1, 2]
     params['destination_location'] = 'latest_model/'
     params['l2_beta'] = 0.02
     params['n_epoch'] = 20000 # 40000
     params['batch_size'] = 10
-    params['learning_rate'] = 0.001
-    params['dropout_prob'] = 0.6
+    params['learning_rate'] = 0.01
     params['outputfile'] = '128_128_point2.ckpt'
-    params['keep_prob'] = 1.0
+    params['keep_prob'] = 1
 
     main(params)
